5f26673986e9ce6e4b88911c1f4b5f7999098570
amar1057 <amar1057@stu.southtexascollege.edu>
2021-11-10 21:51:55 -0600

    basic set up

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,23 +10,23 @@ const loggingOut = () => {
 </script>
 
 <template>
-  <div class="text-indigo-200 bg-red-600">
+  <div class="text-pink-200 bg-pink-500">
     <div class="container flex items-center justify-between mx-auto">
       <h1 class="text-3xl font-thin tracking-tighter">
-        Vue<span class="font-normal">FakeAuth</span>
+        Vue<span class="font-normal">RealAuth</span>
       </h1>
       <nav>
         <ul class="flex space-x-4">
           <router-link to="/">
             <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800"
+              class="px-4 py-8  hover:cursor-pointer hover:bg-pink-300 hover:text-pink-200"
             >
               Home
             </li>
           </router-link>
           <router-link :to="{ name: 'About' }">
             <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800"
+              class="px-4 py-8  hover:cursor-pointer hover:bg-pink-300 hover:text-pink-200"
             >
               About
             </li>
@@ -36,7 +36,7 @@ const loggingOut = () => {
             :to="{ path: '/login', name: 'Login' }"
           >
             <li
-              class="px-4 py-8  hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800"
+              class="px-4 py-8  hover:cursor-pointer hover:bg-pink-300 hover:text-pink-200"
             >
               Login
             </li>
@@ -44,14 +44,14 @@ const loggingOut = () => {
           <div v-else class="flex">
             <router-link :to="{ name: 'Secret' }">
               <li
-                class="px-4 py-8  hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800"
+                class="px-4 py-8  hover:cursor-pointer hover:bg-pink-300 hover:text-pink-200"
               >
                 Secret
               </li>
             </router-link>
             <button @click="loggingOut">
               <li
-                class="px-4 py-8  hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800"
+                class="px-4 py-8  hover:cursor-pointer hover:pink-300 hover:text-pink-200"
               >
                 Logout
               </li>
