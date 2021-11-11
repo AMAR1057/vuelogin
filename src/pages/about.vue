dfcbc23f98db3d8d8369c4da439fb5d93d5e4ab3
AMAR1057 <73682137+AMAR1057@users.noreply.github.com>
2021-11-10 21:32:36 -0600

    Initial commit

---
+++ src/pages/about.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+
+
+<template>
+<div class="flex flex-col  items-center justify-center min-h-screen-nonav">
+    <h2 class="font-extralight text-6xl tracking-tighter">About Page</h2>
+    <img src="../assets/bgabout.png  " alt="about BG"></div>
+
+
+
+</template>
